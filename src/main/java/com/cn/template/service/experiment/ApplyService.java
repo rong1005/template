ae35886a4af1cf4e61578e37880e8db63510c9a8
Libra <rong_1005@163.com>
2014-07-28 18:23:45 +0800

    获取请求信息

--- src/main/java/com/cn/template/service/experiment/ApplyService.java
+++ src/main/java/com/cn/template/service/experiment/ApplyService.java
@@ -9,6 +9,11 @@ import javax.servlet.ServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,6 +27,8 @@ import com.cn.template.repository.form.SelectItemDao;
 import com.cn.template.service.form.FormService;
 import com.cn.template.xutil.enums.ApplyStatus;
 import com.cn.template.xutil.enums.FieldType;
+import com.cn.template.xutil.persistence.DynamicSpecifications;
+import com.cn.template.xutil.persistence.SearchFilter;
 import com.google.common.collect.Maps;
 
 /**
@@ -36,7 +43,7 @@ public class ApplyService {
 	/** 日志信息 */
 	private static final Logger logger = LoggerFactory.getLogger(ApplyService.class);
 	
-	/** 表单信息的业务处理 */
+	/** 委托申请信息的业务处理 */
 	private FormService formService;
 	
 	@Autowired
@@ -66,6 +73,15 @@ public class ApplyService {
 	@Autowired
 	public void setBaseMybatisDao(BaseMybatisDao baseMybatisDao) {
 		this.baseMybatisDao = baseMybatisDao;
+	}
+	
+	/**
+	 * 根据ID获得委托申请记录.
+	 * @param id
+	 * @return
+	 */
+	public Apply getApply(Long id) {
+		return applyDao.findOne(id);
 	}
 
 
@@ -156,7 +172,7 @@ public class ApplyService {
 				
 			}
 			
-			//将数据插入到指定的表单（table）中
+			//将数据插入到指定的委托申请（table）中
 			//insert into ${tableName} (${fieldNames}) values (${fieldValues})
 			Map<String, Object> parameters = Maps.newHashMap();
 			parameters.put("tableName", form.getTableName());
@@ -164,7 +180,68 @@ public class ApplyService {
 			parameters.put("fieldValues", fieldValues.toString());
 			baseMybatisDao.insert(parameters);
 		}
-		
-		
+
 	}
+	
+
+	/**
+	 * 单个删除委托申请记录.
+	 * @param id
+	 */
+	public void deleteForm(Long id) {
+		applyDao.delete(id);
+	}
+
+	/**
+	 * 获得所有的委托申请记录.
+	 * @return
+	 */
+	public List<Apply> getAllForm() {
+		return (List<Apply>) applyDao.findAll();
+	}
+
+	/**
+	 * 获取委托申请记录[查询、分页、排序].
+	 * @param searchParams
+	 * @param pageNumber
+	 * @param pageSize
+	 * @param sortType
+	 * @return
+	 */
+	public Page<Apply> getForm(Map<String, Object> searchParams, int pageNumber, int pageSize,
+			String sortType) {
+		PageRequest pageRequest = buildPageRequest(pageNumber, pageSize, sortType);
+		Specification<Apply> spec = buildSpecification(searchParams);
+		return applyDao.findAll(spec, pageRequest);
+	}
+
+	/**
+	 * 创建分页\排序请求.
+	 * @param pageNumber
+	 * @param pagzSize
+	 * @param sortType
+	 * @return
+	 */
+	private PageRequest buildPageRequest(int pageNumber, int pagzSize, String sortType) {
+		Sort sort = null;
+		if ("auto".equals(sortType)) {
+			sort = new Sort(Direction.ASC, "id");
+		} else if ("name".equals(sortType)) {
+			sort = new Sort(Direction.ASC, "name");
+		}
+		return new PageRequest(pageNumber - 1, pagzSize, sort);
+	}
+
+	/**
+	 * 创建动态查询条件组合.
+	 * @param searchParams
+	 * @return
+	 */
+	private Specification<Apply> buildSpecification(Map<String, Object> searchParams) {
+		Map<String, SearchFilter> filters = SearchFilter.parse(searchParams);
+		Specification<Apply> spec = DynamicSpecifications.bySearchFilter(filters.values(), Apply.class);
+		return spec;
+	}
+	
+	
 }
